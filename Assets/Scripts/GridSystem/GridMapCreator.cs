dbf5ab41690f289c4651bc76fcf1180bab27067f
Abdulkadir Semiz <abdulkadirsemiz@outlook.com>
2024-05-01 15:42:08 +0300

    add grid frame setter

--- Assets/Scripts/GridSystem/GridMapCreator.cs
+++ Assets/Scripts/GridSystem/GridMapCreator.cs
@@ -1,49 +1,54 @@
 using System.Collections.Generic;
-using GridSystem;
 using UnityEngine;
 
-public class GridMapCreator : MonoBehaviour
+namespace GridSystem
 {
-    [SerializeField] private Transform gridMapStartPointer;
-    [SerializeField] private Transform gridFrame;
-    [SerializeField] private GridMapSO gridMapSo;
+    public class GridMapCreator : MonoBehaviour
+    {
+        [SerializeField] private RectTransform gridFrame;
+        [SerializeField] private GridMapSO gridMapSo;
+
+        private const float FrameMargin = .1f;
+        private Vector2 _gridMapStartPoint;
     
-    public Dictionary<Vector2, CellData> CreateGridMap()
-    {
-        var cellDataDictionary= FillCellDataDictionary();
-
-        SetGridFrameSize();
-        
-        return cellDataDictionary;
-    }
-
-    private void SetGridFrameSize()
-    {
-        Vector2 gridFrameSize = new Vector2(gridMapSo.cellSize.x * gridMapSo.gridMapSize.x, gridMapSo.cellSize.y * gridMapSo.gridMapSize.y);
-        //TODO çerçeve boyut ve gerekli diğer ayarlamalar halledilecek.
-    }
-
-    private Dictionary<Vector2, CellData> FillCellDataDictionary()
-    {
-        Dictionary<Vector2, CellData> cellDataDictionary= new Dictionary<Vector2, CellData>();
-        for (int i = 0; i < gridMapSo.gridMapSize.x; i++)
+        public Dictionary<Vector2, CellData> CreateGridMap()
         {
-            for (int j = 0; j < gridMapSo.gridMapSize.y; j++)
-            {
-                Vector2 position = (Vector2)gridMapStartPointer.position +
-                                   new Vector2(i * gridMapSo.cellSize.x, j * gridMapSo.cellSize.y * (-1));
-                var cell = new CellData(position);
-                cellDataDictionary.Add(new Vector2(i, j), cell);
-            }
+            SetGridFrame();
+            var cellDataDictionary= FillCellDataDictionary();
+        
+            return cellDataDictionary;
         }
-        return cellDataDictionary;
-    }
 
-    public Vector2 GetCellIndex(Vector2 worldPosition)
-    {
-        var localPosition= worldPosition - (Vector2)gridMapStartPointer.position;
-        var x = Mathf.FloorToInt((localPosition.x / gridMapSo.cellSize.x) + gridMapSo.cellSize.x * .5f);
-        var y = -1*Mathf.FloorToInt((localPosition.y / gridMapSo.cellSize.y) + gridMapSo.cellSize.y * .5f);
-        return new Vector2(x, y);
+        private void SetGridFrame()
+        {
+            _gridMapStartPoint = new Vector2(gridMapSo.cellSize.x / 2 * (gridMapSo.gridMapSize.x + 1),
+                gridMapSo.cellSize.y / 2 * (gridMapSo.gridMapSize.y + 1));
+            Vector2 gridFrameSize = new Vector2(gridMapSo.cellSize.x * gridMapSo.gridMapSize.x+ FrameMargin, gridMapSo.cellSize.y * gridMapSo.gridMapSize.y+FrameMargin);
+            gridFrame.sizeDelta = gridFrameSize;
+        }
+
+        private Dictionary<Vector2, CellData> FillCellDataDictionary()
+        {
+            Dictionary<Vector2, CellData> cellDataDictionary= new Dictionary<Vector2, CellData>();
+            for (int i = 0; i < gridMapSo.gridMapSize.x; i++)
+            {
+                for (int j = 0; j < gridMapSo.gridMapSize.y; j++)
+                {
+                    Vector2 position = _gridMapStartPoint +
+                                       new Vector2(i * gridMapSo.cellSize.x, j * gridMapSo.cellSize.y * (-1));
+                    var cell = new CellData(position);
+                    cellDataDictionary.Add(new Vector2(i, j), cell);
+                }
+            }
+            return cellDataDictionary;
+        }
+
+        public Vector2 GetCellIndex(Vector2 worldPosition)
+        {
+            var localPosition= worldPosition - _gridMapStartPoint;
+            var x = Mathf.FloorToInt((localPosition.x / gridMapSo.cellSize.x) + gridMapSo.cellSize.x * .5f);
+            var y = -1*Mathf.FloorToInt((localPosition.y / gridMapSo.cellSize.y) + gridMapSo.cellSize.y * .5f);
+            return new Vector2(x, y);
+        }
     }
 }
